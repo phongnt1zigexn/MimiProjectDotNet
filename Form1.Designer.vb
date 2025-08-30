7582ab28ef8192a7c59492f382fc22aade5e951b
phongnt1zigexn <phongnt1@zigexn.vn>
2025-08-30 10:34:44 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtTask = New TextBox()
+        btnAdd = New Button()
+        btnDelete = New Button()
+        btnSave = New Button()
+        btnLoad = New Button()
+        clbTasks = New CheckedListBox()
+        btnClear = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(92, 40)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(57, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "To-do list"
+        ' 
+        ' txtTask
+        ' 
+        txtTask.Location = New Point(92, 78)
+        txtTask.Name = "txtTask"
+        txtTask.Size = New Size(531, 23)
+        txtTask.TabIndex = 1
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(92, 311)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 2
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(182, 311)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(113, 23)
+        btnDelete.TabIndex = 4
+        btnDelete.Text = "Delete selected"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(301, 311)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(113, 23)
+        btnSave.TabIndex = 6
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' btnLoad
+        ' 
+        btnLoad.Location = New Point(420, 311)
+        btnLoad.Name = "btnLoad"
+        btnLoad.Size = New Size(113, 23)
+        btnLoad.TabIndex = 7
+        btnLoad.Text = "Load"
+        btnLoad.UseVisualStyleBackColor = True
+        ' 
+        ' clbTasks
+        ' 
+        clbTasks.CheckOnClick = True
+        clbTasks.FormattingEnabled = True
+        clbTasks.Location = New Point(92, 122)
+        clbTasks.Name = "clbTasks"
+        clbTasks.Size = New Size(531, 130)
+        clbTasks.TabIndex = 8
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(539, 311)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(113, 23)
+        btnClear.TabIndex = 9
+        btnClear.Text = "Clear Completed"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnClear)
+        Controls.Add(clbTasks)
+        Controls.Add(btnLoad)
+        Controls.Add(btnSave)
+        Controls.Add(btnDelete)
+        Controls.Add(btnAdd)
+        Controls.Add(txtTask)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTask As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents clbTasks As CheckedListBox
+    Friend WithEvents btnClear As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports Microsoft.Data.Sqlite
+Imports System.IO
+
+Public Class Form1
+    Private dbFile As String = Path.Combine(Application.StartupPath, "todo.db")
+    Private connString As String = $"Data Source={dbFile}"
+
+    Private Class TaskItem
+        Public Property ID As String
+        Public Property Text As String
+        Public Property IsDone As Boolean
+        Public Overrides Function ToString() As String
+            Return Text
+        End Function
+    End Class
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        EnsureDatabase()
+        LoadTasks()
+    End Sub
+
+    Private Sub LoadTasks()
+        clbTasks.Items.Clear()
+        Using cn As New SQLiteConnection(connString)
+            cn.Open()
+            Dim sql As String = "SELECT ID, TaskName, IsDone FROM Tasks ORDER BY ID"
+            Using cmd As New SQLiteCommand(sql, cn)
+                Using rdr = cmd.ExecuteReader()
+                    While rdr.Read()
+                        Dim item As New TaskItem With {
+                            .Id = rdr("ID").ToString(),
+                            .Text = rdr("TaskName").ToString(),
+                            .IsDone = (Convert.ToInt32(rdr("IsDone")) = 1)
+                        }
+                        clbTasks.Items.Add(item, item.IsDone)
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub EnsureDatabase()
+        If Not File.Exists(dbFile) Then
+            ' file is created automatically when connecting
+        End If
+
+        Using cn As New SqliteConnection(connString)
+            cn.Open()
+            Dim sql As String = "CREATE TABLE IF NOT EXISTS Tasks (" &
+                            "ID TEXT PRIMARY KEY, " &
+                            "TaskName TEXT, " &
+                            "IsDone INTEGER)"
+            Using cmd As New SqliteCommand(sql, cn)
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim taskText = txtTask.Text.Trim()
+        If taskText = "" Then
+            MessageBox.Show("Enter a task")
+            Return
+        End If
+
+        Dim newItem As New TaskItem With {
+            .ID = Guid.NewGuid().ToString(),
+            .Text = taskText,
+            .IsDone = False
+        }
+
+        ' Insert into DB
+        Using cn As New SQLiteConnection(connString)
+            cn.Open()
+            Dim sql As String = "INSERT INTO Tasks (ID, TaskName, IsDone) VALUES (@id, @name, 0)"
+            Using cmd As New SQLiteCommand(sql, cn)
+                cmd.Parameters.AddWithValue("@id", newItem.ID)
+                cmd.Parameters.AddWithValue("@name", newItem.Text)
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+
+        ' Add to UI
+        clbTasks.Items.Add(newItem, False)
+        txtTask.Clear()
+        txtTask.Focus()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If clbTasks.SelectedItem Is Nothing Then
+            MessageBox.Show("Select a task first.")
+            Return
+        End If
+
+        Dim selectedItem As TaskItem = CType(clbTasks.SelectedItem, TaskItem)
+
+        ' Delete from DB
+        Using cn As New SQLiteConnection(connString)
+            cn.Open()
+            Dim sql As String = "DELETE FROM Tasks WHERE ID=@id"
+            Using cmd As New SQLiteCommand(sql, cn)
+                cmd.Parameters.AddWithValue("@id", selectedItem.ID)
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+
+        ' Remove from UI
+        clbTasks.Items.Remove(selectedItem)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Go backward to safely remove
+        For i As Integer = clbTasks.CheckedItems.Count - 1 To 0 Step -1
+            Dim item As TaskItem = CType(clbTasks.CheckedItems(i), TaskItem)
+
+            Using cn As New SQLiteConnection(connString)
+                cn.Open()
+                Dim sql As String = "DELETE FROM Tasks WHERE ID=@id"
+                Using cmd As New SQLiteCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@id", item.ID)
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+
+            clbTasks.Items.Remove(item)
+        Next
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Update completion status to DB
+        For i As Integer = 0 To clbTasks.Items.Count - 1
+            Dim item As TaskItem = CType(clbTasks.Items(i), TaskItem)
+            Dim isDone As Boolean = clbTasks.GetItemChecked(i)
+
+            Using cn As New SQLiteConnection(connString)
+                cn.Open()
+                Dim sql As String = "UPDATE Tasks SET IsDone=@done WHERE ID=@id"
+                Using cmd As New SQLiteCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@done", If(isDone, 1, 0))
+                    cmd.Parameters.AddWithValue("@id", item.ID)
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+        Next
+        MessageBox.Show("Tasks saved to database.")
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        LoadTasks()
+    End Sub
+End Class
